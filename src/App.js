b93cf8a2adc15352c08ccbdd5dc1f9511737a6d5
davidmbedford <david.m.bedford1@gmail.com>
2019-12-20 13:18:33 -0500

    footer succesfully added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+import Footer from './components/footer/index.js';
 import Home from './components/home/index.js';
 
 function App() {
   return (
-    <Home/>
+    <>
+    <Footer />
+    <Home />
+    </>
   );
 }
 
