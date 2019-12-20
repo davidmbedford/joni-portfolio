3958fcef7d94b62c386b70e2c811ff97efe04deb
davidmbedford <david.m.bedford1@gmail.com>
2019-12-20 13:41:16 -0500

    adjusted navbar to more acutely finish the ground structure

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -6,9 +6,10 @@ function Navbar() {
         <div class="nav-wrapper">
           <a href="google.com" class="brand-logo">Logo</a>
           <ul id="nav-mobile" class="right hide-on-med-and-down">
-            <li><a href="sass.html">Sass</a></li>
-            <li><a href="badges.html">Components</a></li>
-            <li><a href="collapsible.html">JavaScript</a></li>
+            <li><a href="">Home</a></li>
+            <li><a href="badges.html">Gear</a></li>
+            <li><a href="collapsible.html">Discography</a></li>
+            <li><a href="collapsible.html">Contact</a></li>
           </ul>
         </div>
       </nav>
