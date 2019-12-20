f2ccb7fea45a76d3f98cd4683224476933ed9093
davidmbedford <david.m.bedford1@gmail.com>
2019-12-20 13:30:54 -0500

    Navbar successfully added - footer moved to right location

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import './App.css';
+import Navbar from './components/navbar/index.js';
 import Footer from './components/footer/index.js';
 import Home from './components/home/index.js';
+
 
 function App() {
   return (
     <>
-    <Footer />
+    <Navbar />
     <Home />
+    <Footer />
     </>
   );
 }
