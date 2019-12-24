2a0fff3c1126b548edeea5969367173675426da7
davidmbedford <david.m.bedford1@gmail.com>
2019-12-24 13:09:24 -0500

    adjustments to gear html formatting

--- src/components/gear/index.js
+++ src/components/gear/index.js
@@ -7,118 +7,84 @@ function Gear() {
     <header className="App-header">
       <img src={logo} className="App-logo" alt="logo" />
       <div className="container">
-      <h2>
-      ((((Gear:))))
-      </h2>
-      <h4>
-        Outboard:
-      </h4>
-      <p>
-        Carvin MX1688
-      </p>
-      <h4>
-        Converters:
-      </h4>
-      <p>  
-        Motu 8 pre es
-        Focusrite Clarett octopre
-      </p>
-      <h4>
-        Programs:
-      </h4>
-      <p>  
-        REAPER
-        Pro-tools
-        LOGIC
-      </p>
-      <h4>
-        Tape Machine:
-      </h4>
-      <p>
-        Otari MX 5050 b3 8 track 1/2
-      </p>
-      <h4>
-        Dynamics:
-      </h4>
-      <p>  
-        Ashly CL 50
-        Ashly CL 52
-        DBX 163 xt x2
-        DBX 263 xt
-        Symtrix 525
-        Orban 414
-        EQ:
-        Orban 626a EQ
-        Peavey EQ
-        Rane EQ
-      </p>
-      <h4>   
-        FX:
-      </h4>
-      <p>
-        Fisher Stereo Expander
-        Alesis microverb
-      </p>
-      <h4>
-        Mics:
-      </h4>
-      <p>
-        Dynamics:
-        Sennheiser MD421 x 2
-        Sennheiser e609
-        Shure SM 57
-        Shure SM 58 X 3
-        Audio Technica Pro 25 x 2
-        Beyerdynamic M88
-        AKG D112
-        EV 635a
-        Various oddball dynamics
-      </p>
-      <p>
-        Ribbon:
-        Stellar RSM 3 x 2
-      </p>
-      <p>
-        Condensors
-        Oktava MK 012 x2
-        OktavaMod MK 219
-        Milab 96b x 2
-        GrooveTubes st69 tube mic
-        CAD E200
-        Audio technica 4033a
-        Audio Technica boundary mic x 2
-      </p>
-      <h4>
-        Instruments:
-      </h4>
-      <p>  
-        1970 Slingerland 5 piece
-        Various snares and cymbals
-        1967~ Ludwig 22 x 14
-        1967 Fender Bandmaster Reverb
-        Sunn Concert Lead red knob
-        Hammond ao 35 conversion (Dr Z carmin Ghia)
-        Ampeg Gemini 2
-        Peavey Classic VT
-      </p>
-      <h4>
-        CABS
-      </h4>
-      <p>
-        2 x 15 sunn cab jbl k140
-        Peavey 2 x 12 cab
-        1x 10 weber cab
-        2x10 bass cab
-      </p>
-      <h4>
-        Guitars:
-      </h4>
-      <p>  
-        Peavey T60
-        Peavey Patriot
-        Hamer Double Cut
-        Vantage Bass
-      </p>
+      <h2>((((Gear:))))</h2>
+      <h4>Outboard</h4>
+        <li>Carvin MX1688</li>
+      
+      <h4>Converters</h4>
+        <li>Motu 8 pre es</li>
+        <li>Focusrite Clarett octopre</li>
+      
+      <h4>Programs</h4>
+        <li>REAPER</li>
+        <li>Pro-tools</li>
+        <li>LOGIC</li>
+      
+      <h4>Tape Machine</h4>
+        <li>Otari MX 5050 b3 8 track 1/2</li>
+      
+      <h4>Dynamics</h4>
+        <li>Ashly CL 50</li>
+        <li>Ashly CL 52</li>
+        <li>DBX 163 xt x2</li>
+        <li>DBX 263 xt</li>
+        <li>Symtrix 525</li>
+        <li>Orban 414</li>
+      
+      <h4>EQ</h4>
+        <li>Orban 626a EQ</li>
+        <li>Peavey EQ</li>
+        <li>Rane EQ</li>
+
+      <h4>FX</h4>
+        <li>Fisher Stereo Expander</li>
+        <li>Alesis microverb</li>
+      
+      <h4>Mics</h4>
+      <p>Dynamics:</p>
+        <li>Sennheiser MD421 x 2</li>
+        <li>Sennheiser e609</li>
+        <li>Shure SM 57</li>
+        <li>Shure SM 58 X 3</li>
+        <li>Audio Technica Pro 25 x 2</li>
+        <li>Beyerdynamic M88</li>
+        <li>AKG D112</li>
+        <li>EV 635a</li>
+        <li>Various oddball dynamics</li>
+      <p>Ribbon:</p>
+        <li>Stellar RSM 3 x 2</li>
+      
+      <p>Condensors</p>
+        <li>Oktava MK 012 x2</li>
+        <li>OktavaMod MK 219</li>
+        <li>Milab 96b x 2</li>
+        <li>GrooveTubes st69 tube mic</li>
+        <li>CAD E200</li>
+        <li>Audio technica 4033a</li>
+        <li>Audio Technica boundary mic x 2</li>
+      
+      <h4>Instruments:</h4>
+        <li>1970 Slingerland 5 piece</li>
+        <li>Various snares and cymbals</li>
+        <li>1967~ Ludwig 22 x 14</li>
+        <li>1967 Fender Bandmaster Reverb</li>
+        <li>Sunn Concert Lead red knob</li>
+        <li>Hammond ao 35 conversion (Dr Z carmin Ghia)</li>
+        <li>Ampeg Gemini 2</li>
+        <li>Peavey Classic VT</li>
+      
+      <h4>Cabs</h4>
+        <li>2 x 15 sunn cab jbl k140</li>
+        <li>Peavey 2 x 12 cab</li>
+        <li>1x 10 weber cab</li>
+        <li>2x10 bass cab</li>
+      
+      <h4>Guitars</h4>
+        <li>Peavey T60</li>
+        <li>Peavey Patriot</li>
+        <li>Hamer Double Cut</li>
+        <li>Vantage Bass</li>
+      
       </div>
       <a
         className="App-link"
