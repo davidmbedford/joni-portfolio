90fed3281ab247079cb1f577d740dc960c4f8a97
davidmbedford <david.m.bedford1@gmail.com>
2019-12-28 16:06:52 -0500

    file structure reordered for screens

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import Navbar from './components/navbar/index.js';
 import Footer from './components/footer/index.js';
-import Home from './components/home/index.js';
-import Gear from './components/gear/index.js';
-import Discography from './components/discography/index.js';
-import Contact from './components/contact/index.js';
+import Home from './screens/home/index.js';
+import Gear from './screens/gear/index.js';
+import Discography from './screens/discography/index.js';
+import Contact from './screens/contact/index.js';
+
 
 
 function App() {
