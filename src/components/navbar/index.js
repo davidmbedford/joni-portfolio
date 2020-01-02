e60a66e6b1c6f19807dce83b55394a8df6a11ab5
davidmbedford <david.m.bedford1@gmail.com>
2020-01-01 22:05:27 -0500

    added components for sidenav menu - need to install materializecss - cleaning branches before install at master

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -5,9 +5,11 @@ import './style.css';
 class Navbar extends Component {
   render() {
     return (
+      <>
         <nav>
         <div className="nav-wrapper">
-          <a href="google.com" className="brand-logo">Earth Songs Recordings</a>
+          <a className="brand-logo">Earth Songs Recordings</a>
+          <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
           <ul id="nav-mobile" className="right hide-on-med-and-down">
             <li><Link to='/'>Home</Link></li>
             <li><Link to='/gear'>Gear</Link></li>
@@ -16,6 +18,15 @@ class Navbar extends Component {
           </ul>
         </div>
       </nav>
+
+      <ul className="sidenav" id="mobile-demo">
+      <li><a className="nav-link" href="#home-sec">Home</a></li>
+      <li><a className="nav-link" href="#portfolio-sec">Portfolio</a></li>
+      <li><a className="nav-link" href="#bio-sec">Bio</a></li>
+      <li><a className="nav-link" href="#skills-sec">Skills</a></li>
+      <li><a className="nav-link" href="#contact-sec">Contact</a></li>
+      </ul>
+      </>
     )
   }
 }
