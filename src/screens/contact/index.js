948c899c9ca96a354acf5e2513b2c67c419e18ef
davidmbedford <david.m.bedford1@gmail.com>
2020-03-05 13:51:47 -0500

    contact form JSX written and formatted

--- src/screens/contact/index.js
+++ src/screens/contact/index.js
@@ -2,26 +2,54 @@ import React from 'react';
 // import logo from '../../logo.svg';
 
 function Contact() {
-    return (
+  return (
     <div className="App" id="contactnav">
-    <header className="App-header">
-      {/* <img src={logo} className="App-logo" alt="logo" /> */}
-      <div className="container">
+      <header className="App-header">
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <div className="container">
 
-      <div className="row">
-        <div className="col s12 headingBorder">
-          <h1>Contact</h1>
+          <div className="row">
+            <div className="col s12 headingBorder">
+              <h1>Contact</h1>
+            </div>
+          </div>
+
+          <div className="container">
+            <div className="row">
+              <div className="col m10 offset-m1 s12">
+                <div className="row">
+                  <form className="col s12">
+                    <div className="row">
+                      <div className="input-field col s12">
+                        <input id="name" type="text" />
+                        <label for="name">Name</label>
+                      </div>
+                      <div className="input-field col s12">
+                        <input id="email" type="email" className="form-input" />
+                        <label for="email">Email</label>
+                      </div>
+                      <div className="input-field col s12">
+                        <textarea id="message" className="materialize-textarea"></textarea>
+                        <label for="message">Message</label>
+                      </div>
+                    </div>
+                    <div className="divider"></div>
+                    <div className="row">
+                      <div className="col m12">
+                        <p className="right-align"><button className="btn btn-large waves-effect waves-light" type="button" name="action">Send Message</button></p>
+                      </div>
+                    </div>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+
         </div>
-      </div>
 
-      <p>Email: earthsongsrecording@gmail.com</p>
-      <p>Phone Number: 666-420-4321</p>
-      <p>Mailing Address: maybe you dont want this one idk</p>
-      </div>
-      
-    </header>
-  </div>
-    );
+      </header>
+    </div>
+  );
 }
 
 export default Contact;
