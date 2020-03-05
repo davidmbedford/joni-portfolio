89cf43e66219144cf90b190d72f479b716a70bde
davidmbedford <david.m.bedford1@gmail.com>
2020-03-05 14:22:42 -0500

    contact form performs core functionality

--- src/screens/contact/index.js
+++ src/screens/contact/index.js
@@ -18,25 +18,29 @@ function Contact() {
             <div className="row">
               <div className="col m10 offset-m1 s12">
                 <div className="row">
-                  <form className="col s12">
+                  <form className="col s12" action="mailto:david.m.bedford1@gmail.com">
                     <div className="row">
                       <div className="input-field col s12">
                         <input id="name" type="text" />
                         <label for="name">Name</label>
                       </div>
                       <div className="input-field col s12">
-                        <input id="email" type="email" className="form-input" />
-                        <label for="email">Email</label>
+                        <input id="email" type="email" className="form-input validate" />
+                        <label for="email">Your Email</label>
                       </div>
-                      <div className="input-field col s12">
+                      {/* <div className="input-field col s12">
                         <textarea id="message" className="materialize-textarea"></textarea>
                         <label for="message">Message</label>
-                      </div>
+                      </div> */}
                     </div>
                     <div className="divider"></div>
                     <div className="row">
                       <div className="col m12">
-                        <p className="right-align"><button className="btn btn-large waves-effect waves-light" type="button" name="action">Send Message</button></p>
+                        <p className="right-align">
+                          <button className="btn btn-large waves-effect waves-light" type="submit" name="action">
+                            Send Message
+                          </button>
+                        </p>
                       </div>
                     </div>
                   </form>
