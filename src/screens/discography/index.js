324c694cd08a82d9fa8768444b133f1586199770
davidmbedford <david.m.bedford1@gmail.com>
2020-03-05 23:57:28 -0500

    improved hovers and colors across - fixed content in gear section

--- src/screens/discography/index.js
+++ src/screens/discography/index.js
@@ -1,81 +1,50 @@
 import React from 'react';
-// import logo from '../../logo.svg';
 import './style.css';
 
 function Discography() {
-    return (
+  return (
     <div className="App" id="discognav">
-    <header className="App-header">
+      <header className="App-header">
 
-      <div className="container">
-        
-      <div className="row">
-        <div className="col s12 headingBorder">
-          <h1>Selected Discography</h1>
-        </div>
-      </div>
+        <div className="container">
 
-      <iframe src="https://bandcamp.com/EmbeddedPlayer/album=2689439826/size=large/bgcol=ffffff/linkcol=63b2cc/tracklist=false/artwork=small/transparent=true/" seamless>
-        <a href="http://aparamountalovesupreme.bandcamp.com/album/crisis-meditations">crisis meditations by a paramount, a love supreme</a>
-        </iframe>
-
-      <iframe src="https://bandcamp.com/EmbeddedPlayer/album=2795895275/size=large/bgcol=ffffff/linkcol=63b2cc/tracklist=false/artwork=small/transparent=true/" seamless>
-        <a href="http://holygifts.bandcamp.com/album/old-joy">Old Joy by Holy Gifts</a>
-        </iframe>
-
-      <iframe src="https://bandcamp.com/EmbeddedPlayer/album=2216066439/size=large/bgcol=ffffff/linkcol=63b2cc/tracklist=false/artwork=small/transparent=true/" seamless>
-        <a href="http://shanepalko.bandcamp.com/album/send-me-across-the-sea-ep">Send me across the sea, EP by Shane Palko</a>
-        </iframe>
-      
-
-        {/* <div className="row">
-          <div className="col s12 l6">
-            <div className="container exampleBox">
-              ...
+          <div className="row">
+            <div className="col s12 headingBorder">
+              <h1>Selected Discography</h1>
             </div>
           </div>
-          <div className="col s12 l6 left-align">
-            <h3>(Band Name)</h3>
-            <p>Genre: Xxxx Yyyyy</p>
-            <p>Credit: Recorded this album live at Zzzzz Studios. Mixed every song except, track 4.</p>
-            <p>Year: 2019</p>
-          </div>
-        </div>
 
-        <div className="row">
-          <div className="col s12 l6">
-            <div className="container exampleBox">
-              ...
+          <div className="row">
+            <div className="col s12 left-align">
+              <h5>Engineered, Recorded and Mixed at Earth Sounds in West Philadelphia || Genre: Black Metal, Screamo </h5>
+              <iframe src="https://bandcamp.com/EmbeddedPlayer/album=2689439826/size=large/bgcol=ffffff/linkcol=63b2cc/tracklist=false/artwork=small/transparent=true/" seamless>
+                <a href="http://aparamountalovesupreme.bandcamp.com/album/crisis-meditations">crisis meditations by a paramount, a love supreme</a>
+              </iframe>
             </div>
           </div>
-          <div className="col s12 l6 left-align">
-            <h3>(Band Name)</h3>
-            <p>Genre: Xxxx Yyyyy</p>
-            <p>Credit: Recorded this album live at Zzzzz Studios. Mixed every song except, track 4.</p>
-            <p>Year: 2019</p>
-          </div>
-        </div>
 
-        <div className="row">
-          <div className="col s12 l6">
-            <div className="container exampleBox">
-              ...
+          <div className="row">
+            <div className="col s12 left-align">
+              <h5>Recorded and Mixed at Headroom Studios in North Philadelphia || Genre: Experimental Indie Rock</h5>
+              <iframe src="https://bandcamp.com/EmbeddedPlayer/album=2795895275/size=large/bgcol=ffffff/linkcol=63b2cc/tracklist=false/artwork=small/transparent=true/" seamless>
+                <a href="http://holygifts.bandcamp.com/album/old-joy">Old Joy by Holy Gifts</a>
+              </iframe>
             </div>
           </div>
-          <div className="col s12 l6 left-align">
-            <h3>(Band Name)</h3>
-            <p>Genre: Xxxx Yyyyy</p>
-            <p>Credit: Recorded this album live at Zzzzz Studios. Mixed every song except, track 4.</p>
-            <p>Year: 2019</p>
+
+          <div className="row">
+            <div className="col s12 left-align">
+              <h5>Recorded and Mixed at Gargi's Room in Western Massachusetts || Genre: Alternative Folk </h5>
+              <iframe src="https://bandcamp.com/EmbeddedPlayer/album=2216066439/size=large/bgcol=ffffff/linkcol=63b2cc/tracklist=false/artwork=small/transparent=true/" seamless>
+                <a href="http://shanepalko.bandcamp.com/album/send-me-across-the-sea-ep">Send me across the sea, EP by Shane Palko</a>
+              </iframe>
+            </div>
           </div>
-        </div> */}
 
-      </div>
-
-    </header>
-    
-  </div>
-    );
+        </div>
+      </header>
+    </div>
+  );
 }
 
 export default Discography;
--- src/screens/gear/index.js
+++ src/screens/gear/index.js
@@ -22,27 +22,26 @@ class Gear extends Component {
               <div className="col s12">
                 <Collapsible accordion>
                   <CollapsibleItem expanded={false} header="Dynamics" node="div">
-                    <p>Sennheiser MD421 x 2</p>
+                    <p>Sennheiser MD421 (2)</p>
                     <p>Sennheiser e609</p>
                     <p>Shure SM 57</p>
-                    <p>Shure SM 58 X 3</p>
-                    <p>Audio Technica Pro 25 x 2</p>
+                    <p>Shure SM 58 (3)</p>
                     <p>Beyerdynamic M88</p>
                     <p>AKG D112</p>
                     <p>EV 635a</p>
                     <p>Various oddball dynamics</p>
                   </CollapsibleItem>
                   <CollapsibleItem expanded={false} header="Ribbon" node="div">
-                    <p>Stellar RSM 3 x 2</p>
+                    <p>Stellar RSM 3 (2)</p>
                   </CollapsibleItem>
                   <CollapsibleItem expanded={false} header="Condensors" node="div">
-                    <p>Oktava MK 012 x2</p>
+                    <p>Oktava MK 012 (2)</p>
                     <p>OktavaMod MK 219</p>
-                    <p>Milab 96b x 2</p>
+                    <p>Milab 96b (2)</p>
                     <p>GrooveTubes st69 tube mic</p>
                     <p>CAD E200</p>
                     <p>Audio technica 4033a</p>
-                    <p>Audio Technica boundary mic x 2</p>
+                    <p>Audio Technica boundary mic (2)</p>
                   </CollapsibleItem>
                 </Collapsible>
               </div>
@@ -54,19 +53,17 @@ class Gear extends Component {
                   <CollapsibleItem expanded={false} header="Drums" node="div">
                     <p>1970 Slingerland 5 piece</p>
                     <p>Various snares and cymbals</p>
-                    <p>1967~ Ludwig 22 x 14</p>
                   </CollapsibleItem>
                   <CollapsibleItem expanded={false} header="Amps" node="div">
                     <p>1967 Fender Bandmaster Reverb</p>
                     <p>Sunn Concert Lead red knob</p>
                     <p>Hammond ao 35 conversion (Dr Z carmin Ghia)</p>
                     <p>Ampeg Gemini 2</p>
-                    <p>Peavey Classic VT</p>
                   </CollapsibleItem>
                   <CollapsibleItem expanded={false} header="Cabs" node="div">
-                    <p>2 x 15 sunn cab jbl k140</p>
+                    <p>2x15 Sunn cab jbl k140</p>
                     <p>Peavey 2 x 12 cab</p>
-                    <p>1x 10 weber cab</p>
+                    <p>1x10 Weber cab</p>
                     <p>2x10 bass cab</p>
                   </CollapsibleItem>
                   <CollapsibleItem expanded={false} header="Guitars" node="div">
@@ -74,6 +71,14 @@ class Gear extends Component {
                     <p>Peavey Patriot</p>
                     <p>Hamer Double Cut</p>
                     <p>Vantage Bass</p>
+                  </CollapsibleItem>
+                  <CollapsibleItem expanded={false} header="Effects Pedals" node="div">
+                    <p>Sans Amp</p>
+                    <p>Source Audio Ventris</p>
+                    <p>Dirt Transmitter</p>
+                    <p>Land Devices HP 2</p>
+                    <p>Electro Harmonix Analog Boy</p>
+                    <p>Empress Super Delay</p>
                   </CollapsibleItem>
                 </Collapsible>
               </div>
@@ -101,7 +106,7 @@ class Gear extends Component {
                   <CollapsibleItem expanded={false} header="Dynamics" node="div">
                     <p>Ashly CL 50</p>
                     <p>Ashly CL 52</p>
-                    <p>DBX 163 xt x2</p>
+                    <p>DBX 163 xt (2)</p>
                     <p>DBX 263 xt</p>
                     <p>Symtrix 525</p>
                     <p>Orban 414</p>
@@ -113,7 +118,7 @@ class Gear extends Component {
                   </CollapsibleItem>
                   <CollapsibleItem expanded={false} header="FX" node="div">
                     <p>Fisher Stereo Expander</p>
-                    <p>Alesis microverb</p>
+                    <p>Alesis Microverb</p>
                   </CollapsibleItem>
                 </Collapsible>
               </div>
