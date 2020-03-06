0fc3595e7fff8c96021d553ca3990dc3c3b025ef
davidmbedford <david.m.bedford1@gmail.com>
2020-03-05 22:26:10 -0500

    edits to discog made, need to add text but all 3 players are there

--- src/screens/discography/index.js
+++ src/screens/discography/index.js
@@ -6,7 +6,6 @@ function Discography() {
     return (
     <div className="App" id="discognav">
     <header className="App-header">
-      {/* <img src={logo} className="App-logo" alt="logo" /> */}
 
       <div className="container">
         
@@ -16,7 +15,20 @@ function Discography() {
         </div>
       </div>
 
-        <div className="row">
+      <iframe src="https://bandcamp.com/EmbeddedPlayer/album=2689439826/size=large/bgcol=ffffff/linkcol=63b2cc/tracklist=false/artwork=small/transparent=true/" seamless>
+        <a href="http://aparamountalovesupreme.bandcamp.com/album/crisis-meditations">crisis meditations by a paramount, a love supreme</a>
+        </iframe>
+
+      <iframe src="https://bandcamp.com/EmbeddedPlayer/album=2795895275/size=large/bgcol=ffffff/linkcol=63b2cc/tracklist=false/artwork=small/transparent=true/" seamless>
+        <a href="http://holygifts.bandcamp.com/album/old-joy">Old Joy by Holy Gifts</a>
+        </iframe>
+
+      <iframe src="https://bandcamp.com/EmbeddedPlayer/album=2216066439/size=large/bgcol=ffffff/linkcol=63b2cc/tracklist=false/artwork=small/transparent=true/" seamless>
+        <a href="http://shanepalko.bandcamp.com/album/send-me-across-the-sea-ep">Send me across the sea, EP by Shane Palko</a>
+        </iframe>
+      
+
+        {/* <div className="row">
           <div className="col s12 l6">
             <div className="container exampleBox">
               ...
@@ -56,7 +68,7 @@ function Discography() {
             <p>Credit: Recorded this album live at Zzzzz Studios. Mixed every song except, track 4.</p>
             <p>Year: 2019</p>
           </div>
-        </div>
+        </div> */}
 
       </div>
 
