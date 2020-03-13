ea13f6d1759e6c7ee576abf6d583d06eb55cad7b
davidmbedford <david.m.bedford1@gmail.com>
2020-03-13 13:38:41 -0400

    meta data improved

--- src/screens/discography/index.js
+++ src/screens/discography/index.js
@@ -18,10 +18,9 @@ function Discography() {
               <div className="col s12 left-align">
                 <h5>Engineered, Recorded and Mixed at Earth Sounds in West Philadelphia || Genre: Black Metal, Screamo </h5>
                 <iframe className="bandcamp" 
-                  src="https://bandcamp.com/EmbeddedPlayer/album=2689439826/size=large/bgcol=ffffff/linkcol=63b2cc/tracklist=false/artwork=small/transparent=true/" 
-                  samesite="None Secure" 
+                  src="https://bandcamp.com/EmbeddedPlayer/album=2689439826/size=large/bgcol=ffffff/linkcol=63b2cc/tracklist=false/artwork=small/transparent=true/"  
                   title="A Paramount A Love Supreme Bandcamp Page" seamless>
-                  <a href="http://aparamountalovesupreme.bandcamp.com/album/crisis-meditations" samesite="None Secure">crisis meditations by a paramount, a love supreme</a>
+                  <a href="http://aparamountalovesupreme.bandcamp.com/album/crisis-meditations">crisis meditations by a paramount, a love supreme</a>
                 </iframe>
               </div>
             </div>
@@ -31,9 +30,8 @@ function Discography() {
                 <h5>Recorded and Mixed at Headroom Studios in North Philadelphia || Genre: Experimental Indie Rock</h5>
                 <iframe className="bandcamp" 
                   src="https://bandcamp.com/EmbeddedPlayer/album=2795895275/size=large/bgcol=ffffff/linkcol=63b2cc/tracklist=false/artwork=small/transparent=true/" 
-                  samesite="None Secure" 
                   title="Holy Gifts Bandcamp Page" seamless>
-                  <a href="http://holygifts.bandcamp.com/album/old-joy" samesite="None Secure">Old Joy by Holy Gifts</a>
+                  <a href="http://holygifts.bandcamp.com/album/old-joy">Old Joy by Holy Gifts</a>
                 </iframe>
               </div>
             </div>
@@ -43,7 +41,6 @@ function Discography() {
                 <h5>Recorded and Mixed at Supermagic Studios in Landenburg PA || Genre: Alternative Folk </h5>
                 <iframe id="youtube" 
                   src="https://www.youtube.com/embed/jsJlCgeZLuQ" 
-                  samesite="None Secure" 
                   allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" 
                   title="Shane Palko Youtube Page"></iframe>
               </div>
