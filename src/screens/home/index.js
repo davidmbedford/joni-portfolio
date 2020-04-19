3805c7080d164895e435613f3e6470f0e618d21d
davidmbedford <david.m.bedford1@gmail.com>
2020-04-18 20:01:40 -0400

    (bug) pictures moved in an ungraceful manner - xml edited to ensure smoother placement on phone rotation

--- src/screens/home/index.js
+++ src/screens/home/index.js
@@ -9,21 +9,32 @@ function Home() {
     <div className="App" id="homenav">
       <div className="App-header">
         <div className="container">
-          <p className="homeParagraph"> <img src={Tape} className="picRight" alt="analog tape machine" />
+
+        <div className="row">
+          <img src={Tape} className="picRight" alt="analog tape machine" />
+          <p className="homeParagraph"> 
             Joni Elfers is a sonic facilitator and audio engineer. She is experienced with tracking, mixing, and mastering. She puts her emphasis on
             connecting with the artist and what they want. Recording is a sacred process. Music connects people in a deep and intangible way,
             and Joni wants to respect and honor that in the creative process.
           </p>
-          <p className="homeParagraph"> <img src={SilverDrum} className="picLeft" alt="silver drumset" />
+        </div>
+        <div className="row">
+          <img src={SilverDrum} className="picLeft" alt="silver drumset" />
+          <p className="homeParagraph"> 
             She is well-versed in both digital and traditional analog techniques. She also works with field sounds and found sounds.
             Joni has been recording bands since high school, and got into the music industry though the DIY music scene. Over time, she has since developed a deep love
             and appreciation for a large variety of music.  Her output reflects this, having done everything from doom metal to orchestrated folk records,
             and even ambient and found sounds songs.
           </p>
-          <p className="homeParagraph"> <img src={GoldDrum} className="picRight" alt="gold drumset" />
+        </div>
+        <div className="row">
+          <img src={GoldDrum} className="picRight" alt="gold drumset" />
+          <p className="homeParagraph"> 
             She currently does not have a studio set-up, but is able to run sessions from any available studio in your area. She also does mobile
             recording and can do it in almost any location, even those without power.
           </p>
+        </div>
+
         </div>
       </div>
     </div>
